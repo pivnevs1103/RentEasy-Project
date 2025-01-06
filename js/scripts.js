e61bbaa06b048e1648ba5017fa81f7cbcd407ec8
Sergey Pivnev <pivnevs1103@gmail.com>
2025-01-06 02:27:03 -0500

    Finished the front-end

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,36 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+    const create = document.getElementById("create");
+    const login = document.getElementById("login");
+    const signout = document.getElementById("signout");
+
+    login.addEventListener("click", () => {
+        alert("Success! You have logged In!");
+        window.location.href="/index.html";
+    });
+    
+    create.addEventListener("click", (event) => {
+        alert("Success! You have created an account!");
+        window.location.href="/index.html";
+        event.stopImmediatePropagation();
+    });
+
+    login.addEventListener("click", () => {
+        alert("Success! You have logged In!");
+        window.location.href="/index.html";
+    });
+
+
+    signout.addEventListener("click", () => {
+        alert("You have successfully signed out!");
+    });
+
+const priceSlider = document.getElementById("price");
+const priceDisplay = document.getElementById("price-display");
+
+// Add an event listener to the slider
+priceSlider.addEventListener("input", () => {
+    priceDisplay.textContent = priceSlider.value;
+});
